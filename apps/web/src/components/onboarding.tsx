55f795971ea14efe7e8bf97bcff33a5feca6a11d
Nizav <nizav.business@gmail.com>
2025-07-20 19:17:55 +0700

    fix: onboarding not exported correctly

--- apps/web/src/components/onboarding.tsx
+++ apps/web/src/components/onboarding.tsx
@@ -9,22 +9,19 @@ import {
 } from "./ui/dialog";
 import { useTranslation } from "react-i18next";
 
-interface OnboardingProps {
-	isOpen: boolean;
-	onClose: () => void;
-}
 
-	const { t } = useTranslation("common");
-	return (
-		<Dialog open={isOpen} onOpenChange={onClose}>
-			<DialogContent>
-				<DialogHeader>
-					<DialogTitle>{t("onboarding_welcome", "Welcome to Clipture")}</DialogTitle>
-					<DialogDescription>
-						{t("onboarding_description", "Let's get you started with the basics.")}
-					</DialogDescription>
-				</DialogHeader>
-			</DialogContent>
-		</Dialog>
-	);
+export function Onboarding({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) {
+	   const { t } = useTranslation("common");
+	   return (
+			   <Dialog open={isOpen} onOpenChange={onClose}>
+					   <DialogContent>
+							   <DialogHeader>
+									   <DialogTitle>{t("onboarding_welcome", "Welcome to Clipture")}</DialogTitle>
+									   <DialogDescription>
+											   {t("onboarding_description", "Let's get you started with the basics.")}
+									   </DialogDescription>
+							   </DialogHeader>
+					   </DialogContent>
+			   </Dialog>
+	   );
 }
