8f222edddec9c4e21e6f6c4baaffa753f26bbe2b
Abubakarashrafi <abubakar16350@gmail.com>
2025-07-22 21:05:33 +0500

    fix: clear old project ui and load new project after creation

--- apps/web/src/components/editor/media-panel/views/audio.tsx
+++ apps/web/src/components/editor/media-panel/views/audio.tsx
@@ -4,15 +4,15 @@ import { Input } from "@/components/ui/input";
 import { useState } from "react";
 
 export function AudioView() {
-  const [search, setSearch] = useState("");
-  return (
-    <div className="h-full flex flex-col gap-2 p-4">
-      <Input
-        placeholder="Search songs and artists"
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-      />
-      <div className="flex flex-col gap-2"></div>
-    </div>
-  );
+	const [search, setSearch] = useState("");
+	return (
+		<div className="h-full flex flex-col gap-2 p-4">
+			<Input
+				placeholder="Search songs and artists"
+				value={search}
+				onChange={(e) => setSearch(e.target.value)}
+			/>
+			<div className="flex flex-col gap-2"></div>
+		</div>
+	);
 }
