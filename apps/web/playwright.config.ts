942c52d556a27e86795cf25462b738317d919168
donghaozhang <zdhpeter@gmail.com>
2025-07-29 21:17:26 +1000

    Update documentation and project organization

--- apps/web/playwright.config.ts
+++ apps/web/playwright.config.ts
@@ -5,6 +5,7 @@ import { defineConfig, devices } from '@playwright/test'
  */
 export default defineConfig({
   testDir: './e2e',
+  testIgnore: '**/unit-tests-converted/**',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
